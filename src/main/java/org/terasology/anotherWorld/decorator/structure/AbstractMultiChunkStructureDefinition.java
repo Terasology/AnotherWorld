b07e9ad4f59bc4bc645eb39389cf6f3bceaaf711
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-01-10 20:08:48 +0100

    feat(JOML): migrate AnotherWorld

--- src/main/java/org/terasology/anotherWorld/decorator/structure/AbstractMultiChunkStructureDefinition.java
+++ src/main/java/org/terasology/anotherWorld/decorator/structure/AbstractMultiChunkStructureDefinition.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.anotherWorld.decorator.structure;
 
 import org.joml.Vector3i;
@@ -59,7 +46,7 @@ public abstract class AbstractMultiChunkStructureDefinition implements Structure
     }
 
     protected final void generateStructuresForChunkWithFrequency(List<Structure> result, long seed, Vector3i chunkPosition, Vector3i chunkSize) {
-        Random random = ChunkRandom.getChunkRandom(seed, JomlUtil.from(chunkPosition), getGeneratorSalt());
+        Random random = ChunkRandom.getChunkRandom(seed, chunkPosition, getGeneratorSalt());
 
         float structuresInChunk = frequency.getValue(random);
         int structuresToGenerateInChunk = (int) structuresInChunk;
